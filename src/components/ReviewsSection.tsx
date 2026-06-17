8de68aa8c7abe557b6cba4bd98303de31ec23f70
Diversplas Dev <dev@diversplas.com>
2026-06-18 00:37:55 +0200

    Add black stroke to stars in ReviewsSection

--- src/components/ReviewsSection.tsx
+++ src/components/ReviewsSection.tsx
@@ -20,7 +20,7 @@ function StarRating() {
     <div className="flex gap-1">
       {[1, 2, 3, 4, 5].map((star) => (
         <svg key={star} className="w-5 h-5 text-[#D8E600] fill-current" viewBox="0 0 24 24">
-          <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+          <path stroke="black" strokeWidth="1.5" strokeLinejoin="round" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
         </svg>
       ))}
     </div>
